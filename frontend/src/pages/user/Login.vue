0f87235ea190ea750eb5cc01ec3058a846166ff7
double <bud2@qq.com>
2017-12-29 16:40:05 +0800

    login 组件

--- frontend/src/pages/user/Login.vue
+++ frontend/src/pages/user/Login.vue
@@ -1,68 +1,68 @@
 <<template lang="html">
   <el-form :model="loginForm" class="login-container" :rules="loginRule">
-	<h3 class="title">forrest的blog</h3>
-	<el-form-item prop="email">
-		<el-input type="text" v-model="loginForm.email" placeholder="电子邮箱", auto-complete="off"></el-input>
-	</el-form-item>
-	<el-form-item prop="password">
-		<el-input v-model="loginForm.password" placeholder="密码"></el-input>
-	</el-form-item>
-	<el-form-item>
-		<el-button type="primary" @click.native.prevent="handleLogin" :loading="logining">
-			登录
-		</el-button>
-	</el-form-item>
+  <h3 class="title">forrest的blog</h3>
+  <el-form-item prop="email">
+    <el-input type="text" v-model="loginForm.email" placeholder="电子邮箱", auto-complete="off"></el-input>
+  </el-form-item>
+  <el-form-item prop="password">
+    <el-input v-model="loginForm.password" placeholder="密码"></el-input>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click.native.prevent="handleLogin" :loading="logining">
+      登录
+    </el-button>
+  </el-form-item>
   </el-form>
 </template>
 <script>
-	import { requestLogin } from "@/api";
-	
-	export default {
-		name: 'userLogin',
-		data () {
-			return {
-				logining: false,
-				loginForm: {
-					email: '',
-					password: '',
-				},
-				loginRule: {
-					email: [
-						{requierd: true, message: '请输入电子邮箱', trigger: 'blur' }
-					],
-					password: [
-						{required: true, message: '请输入密码', trigger: 'blur'}
-					]
-				}
-			}
-		},
-		methods: {
-			handleLogin () {
-				this.$refs.loginForm.validate((valid) => {
-					if (valid) {
-						this.logining = true
-						let loginParams = {email: this.loginForm.email, password: this.loginForm.password}
-						requestLogin(loginParams).then(data => {
-							this.login = false
-							let {msg, code, token} = data 
-							if (code !== 0) {
-								this.$msg({
-									msg: msg,
-									type: 'error'	
-								})
-							} else {
-								this.$router.push({path: '/'})
-								ssionStorage.setItem('token', JSON.stringify(token))
-							}
-						})
-					} else {
-						return false
-					}
-				}
-				)
-			}
-		}
-	}
+  import { requestLogin } from '../../api'
+  
+  export default {
+    name: 'UserLogin',
+    data () {
+      return {
+        logining: false,
+        loginForm: {
+          email: '',
+          password: ''
+        },
+        loginRule: {
+          email: [
+            {requierd: true, message: '请输入电子邮箱', trigger: 'blur'}
+          ],
+          password: [
+            {required: true, message: '请输入密码', trigger: 'blur'}
+          ]
+        }
+      }
+    },
+    methods: {
+      handleLogin () {
+        this.$refs.loginForm.validate((valid) => {
+          if (valid) {
+            this.logining = true
+            let loginParams = {email: this.loginForm.email, password: this.loginForm.password}
+            requestLogin(loginParams).then(data => {
+              this.login = false
+              let {msg, code} = data
+              if (code !== 0) {
+                this.$msg({
+                  msg: msg,
+                  type: 'error'
+                })
+              } else {
+                this.$router.push({path: '/'})
+                // ssionStorage.setItem('token', JSON.stringify(token))
+              }
+            })
+          } else {
+            return false
+          }
+        }
+        )
+      }
+    }
+  }
 </script>
 <<style lang="scss" scoped>
   .login-container {
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // 用户
-// import userLogin from '@/pages/user/Login.vue'
+import UserLogin from '@/pages/user/Login.vue'
 
 Vue.use(Router)
 export default new Router({
   routes: [
     {
       path: '/login',
-      // component: userLogin,
+      component: UserLogin,
       name: 'Login'
     }
   ]
