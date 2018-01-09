f3f722c6bfecc84c7fa376fb2f3e8301850e8d05
double <bud2@qq.com>
2018-01-09 15:17:47 +0800

    Delete app_name

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/v1/', include(("apps.accounts.urls")))
+    url(r'^api/v1/user/', include(("apps.accounts.urls")))
 ]
